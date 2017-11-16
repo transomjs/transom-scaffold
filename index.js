67b642b7b000bc4da3bfefe1691d20de0bda435f
Mark Voorberg <mark@binaryops.ca>
2017-11-15 18:21:05 -0800

    added static asset support

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const ScaffoldHandler = require('./lib/scaffoldHandler');
 
 function TransomScaffold() {
 	this.initialize = function (server, options) {
-		server.registry.set(options.registryKey || 'transomScaffold', new ScaffoldHandler(server, options));
+        const apiScaffold = server.registry.get('transom-options.api_definition.scaffold', {});
+		const handlerOpts = Object.assign({}, apiScaffold, options);
+		const scaffoldHandler = new ScaffoldHandler(server, handlerOpts);
+		server.registry.set(options.registryKey || 'transomScaffold', scaffoldHandler);
 	}
 }
 
