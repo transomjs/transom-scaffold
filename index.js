1f4e9b5ad306575fd4fc13581dcc353e2f98ac86
Mark Voorberg <mark@binaryops.ca>
2017-11-24 21:57:02 -0800

    api definition changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ScaffoldHandler = require('./lib/scaffoldHandler');
 
 function TransomScaffold() {
 	this.initialize = function (server, options) {
-        const apiScaffold = server.registry.get('transom-options.api_definition.scaffold', {});
+        const apiScaffold = server.registry.get('transom-config.definition.scaffold', {});
 		options.scaffold = Object.assign({}, apiScaffold, options.scaffold);
 		
 		const scaffoldHandler = new ScaffoldHandler(server, options);
