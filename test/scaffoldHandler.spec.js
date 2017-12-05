bec11312ff8e38a84181a57d2fc7e690ff8777b6
Mark Voorberg <mark@binaryops.ca>
2017-12-04 17:52:09 -0800

    Adding deps and some tests

--- test/scaffoldHandler.spec.js
+++ test/scaffoldHandler.spec.js
@@ -1,20 +1,91 @@
 "use strict";
 const expect = require('chai').expect;
 const sinon = require('sinon');
+const path = require('path');
 const ScaffoldHandler = require('../lib/scaffoldHandler');
 
-describe('SmtpHandler', function () {
+describe('ScaffoldHandler', function () {
 
-    beforeEach(function () {
+    // beforeEach(function () {
+    // });
+
+    // afterEach(function () {
+    // });
+
+    it('should be an Object with three methods', function () {
+        var dummyServer = {};
+        dummyServer.get = sinon.spy();
+
+        const scaffoldHandler = new ScaffoldHandler(dummyServer, {});
+
+        expect(scaffoldHandler).to.be.an.instanceOf(Object);
+        expect(scaffoldHandler.addStaticAssetRoute).to.be.an.instanceOf(Function);
+        expect(scaffoldHandler.addRedirectRoute).to.be.an.instanceOf(Function);
+        expect(scaffoldHandler.addTemplateRoute).to.be.an.instanceOf(Function);
+        // expect(Object.keys(scaffoldHandler).length).to.be.equal(3);
     });
 
-    afterEach(function () {
-    });
-    
-    it('should have tests!', function () {
-        const dummyServer = {};
-        const dummyOptions = {};
+    it('should setup three \'get\' routes on the server', function () {
 
-        expect(true).to.be.true;
+        const reqHandler = function (req, res, next) {
+            console.log("args:", arguments);
+        };
+
+        var dummyServer = {};
+        dummyServer.get = sinon.spy(function (uri, reqHandler) {
+            console.log(arguments);
+        });
+
+        const scaffoldHandler = new ScaffoldHandler(dummyServer, {});
+        const key = "foo";
+        const scaffold = {
+            serveStatic: sinon.spy(function (options) {
+                return options;
+            })
+        };
+        scaffoldHandler.addStaticAssetRoute(dummyServer, "one", scaffold);
+        scaffoldHandler.addRedirectRoute(dummyServer, "two", scaffold);
+        scaffoldHandler.addTemplateRoute(dummyServer, "three", scaffold);
+
+        expect(dummyServer.get.calledThrice).to.be.true;
+
+        dummyServer.get('one');
+        expect(scaffold.serveStatic.calledOnce).to.be.true;
+        const args = scaffold.serveStatic.args[0][0]; // first call, first arg.
+
+        expect(args.appendRequestPath).to.be.true;
+        expect(args.default).to.equal("index.html");
+        const testPath = path.join(__dirname, "..", "one");
+        expect(args.directory).to.equal(testPath);
+
     });
+
+    it('should setup a /automobile route on the server', function () {
+
+        var dummyServer = {};
+        dummyServer.get = sinon.spy(function (uri) {});
+
+        const scaffoldHandler = new ScaffoldHandler(dummyServer, {});
+        // const key = "foobar";
+        const scaffold = {
+            serveStatic: sinon.spy(function (options) {
+                return options;
+            }),
+            path: '/assets/a*',
+            defaultAsset: 'default.png'
+        };
+        scaffoldHandler.addStaticAssetRoute(dummyServer, "horse", scaffold);
+        expect(dummyServer.get.calledOnce).to.be.true;
+
+        dummyServer.get('/assets/foo.jpg');
+        expect(scaffold.serveStatic.calledOnce).to.be.true;
+        const args = scaffold.serveStatic.args[0][0]; // first call, first arg.
+
+        expect(args.appendRequestPath).to.be.true;
+        expect(args.default).to.equal("default.png");
+        const testPath = path.join(__dirname, "..", "horse");
+        expect(args.directory).to.equal(testPath);
+
+    });
+
 });
