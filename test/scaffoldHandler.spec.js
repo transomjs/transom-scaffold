b711926253114e7624a25e7fdb23a8519b14ba2b
Mark Voorberg <mark@binaryops.ca>
2017-12-10 18:30:16 -0800

    more tests

--- test/scaffoldHandler.spec.js
+++ test/scaffoldHandler.spec.js
@@ -97,10 +97,75 @@ describe('ScaffoldHandler', function () {
             path: '/myPath',
             redirect: '/redirectPath'
         };
-        scaffoldHandler.addRedirectRoute(dummyServer, "piglet", scaffold);
+        scaffoldHandler.addRedirectRoute(dummyServer, "chicken", scaffold);
         expect(dummyServer.get.calledOnce).to.be.true;
         const uri = dummyServer.get.args[0][0]; // first call, first arg.
         expect(uri).to.be.equal('/myPath');
+
+        const fn = dummyServer.get.args[0][1]; // first call, second arg.
+        const res = {
+            redirect: sinon.spy()
+        };
+        const next = sinon.spy();
+        fn({}, res, next);
+        expect(res.redirect.calledOnce).to.be.true;
+        const redirectArgs = res.redirect.args[0];
+        expect(redirectArgs[0]).to.equal(scaffold.redirect);
+    });
+
+    it('should setup a template route with data and headers', function (done) {
+        var dummyServer = {
+            registry: {
+                get: function () {
+                    return {
+                        renderHtmlTemplate: function (templateName, data) {
+                            let result = templateName;
+                            Object.keys(data).sort().map((key) => {
+                                result += `\n${key}:${data[key]}`;
+                            });
+                            return result;
+                        }
+                    }
+                }
+            }
+        };
+        dummyServer.get = sinon.spy();
+
+        const scaffoldHandler = new ScaffoldHandler(dummyServer, {});
+        const scaffold = {
+            contentType: 'apple/pear',
+            templateName: 'fooTemplate',
+            data: {
+                foo: 123,
+                bar: 'baz'
+            }
+        };
+        scaffoldHandler.addTemplateRoute(dummyServer, "hippo", scaffold);
+        expect(dummyServer.get.calledOnce).to.be.true;
+        const uri = dummyServer.get.args[0][0]; // first call, first arg.
+        expect(uri).to.be.equal('/hippo');
+        const fn = dummyServer.get.args[0][1];
+        const req = {
+            headers: "jazz",
+            params: "cookie"
+        };
+        const res = {
+            setHeader: sinon.spy(),
+            end: sinon.spy(),
+        };
+
+        fn(req, res, function () {
+            expect(res.setHeader.calledOnce).to.be.true;
+            const setHeaderArgs = res.setHeader.args[0];
+            expect(setHeaderArgs[0]).to.equal('content-type');
+            expect(setHeaderArgs[1]).to.equal(scaffold.contentType);
+
+            expect(res.end.calledOnce).to.be.true;
+            const endArgs = res.end.args[0];
+            const dummy = 'fooTemplate\nbar:baz\nfoo:123\nheaders:jazz\nparams:cookie';
+            expect(endArgs[0]).to.equal(dummy);
+            done();
+        });
     });
 
 });
