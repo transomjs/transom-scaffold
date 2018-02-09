8c8f9ec8e0ead6ee9f9c07f12717883c4fc666c3
Wiebo Troost <wiebo@binaryops.ca>
2018-02-08 16:41:22 -0800

    return a promise from the initialize

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const ScaffoldHandler = require('./lib/scaffoldHandler');
 
 function TransomScaffold() {
 	this.initialize = function (server, options) {
-        const apiScaffold = server.registry.get('transom-config.definition.scaffold', {});
-		options.scaffold = Object.assign({}, apiScaffold, options.scaffold);
-		
-		const scaffoldHandler = new ScaffoldHandler(server, options);
-		server.registry.set(options.registryKey || 'transomScaffold', scaffoldHandler);
+		return new Promise(function(resolve, reject){
+			const apiScaffold = server.registry.get('transom-config.definition.scaffold', {});
+			options.scaffold = Object.assign({}, apiScaffold, options.scaffold);
+			
+			const scaffoldHandler = new ScaffoldHandler(server, options);
+			server.registry.set(options.registryKey || 'transomScaffold', scaffoldHandler);
+			resolve();
+		});
 	}
 }
 
